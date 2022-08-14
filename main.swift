53ee9ecf4799686896c170b8fadf9377ced989a8
Sg24n <47612337+Sg24n@users.noreply.github.com>
2022-08-14 23:43:43 +0200

    Glory

--- main.swift
+++ main.swift
@@ -1,7 +1,12 @@
+print("А чё писать... ?\nНажми цифру 2🥺")
+
 var a  = readLine()
+
 switch a {
-case "1": print("yr mom gay")
-    
-default: print("† Бооже спаси и сохрани")
-    
+case "1": print("YoUre mom gay")
+
+ case "2": print("Glory to Ukraїne")
+
+default: print("† Бооже спаси и сохрани")  
+
 }
