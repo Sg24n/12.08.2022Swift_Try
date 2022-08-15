cd80a28a9ded32210cef10d216b2b3f205efa689
Sg24n <47612337+Sg24n@users.noreply.github.com>
2022-08-16 00:08:32 +0200

    Инкубаторный Порыв

--- main.swift
+++ main.swift
@@ -1,12 +1,37 @@
-print("А чё писать... ?\nНажми цифру 2🥺")
+print("Choose ur champion\n 1 - First\n 2 - Livfer")
+let insert = readLine()
 
-var a  = readLine()
+switch insert {
+case "1": firstFunc()
+case "2": Inkubatorniy()
+default: print("Такова нема")
+    
+}
 
-switch a {
-case "1": print("YoUre mom gay")
+func firstFunc() {
 
- case "2": print("Glory to Ukraїne")
+    print("А чё писать... ?\nНажми цифру 2🥺")
 
-default: print("† Бооже спаси и сохрани")  
+    let a  = readLine()
+
+    switch a {
+    case "1": print("YoUre mom gay")
+    case "2": print("Glory to Ukraїne")
+    default: print("† Бооже спаси и сохрани")  
+
+}
+
+}
+
+func Inkubatorniy() {
+    print("Укажи год смерти инкубаторного:")
+    if let inserted = readLine(){
+       if let date = Int(inserted){
+           let death = date - 2004
+            print("Инкуботорный проживёт \(death) years\nПочему уеарс? потому что мне лень делать обработчик\n и миллион вариантов написания год годов года годовавов и т.д.\n И вообще программирование на английском всё да😤")
+
+       }
+    }
+
 
 }
